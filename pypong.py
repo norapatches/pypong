f9f9a49add445cce70c1e9f714a0a7b4d9675d85
norapatches <info@norapatches.com>
2024-03-04 20:40:29 +0100

    Added About window and improved on game mechanics

--- pypong.py
+++ pypong.py
@@ -2,7 +2,7 @@ import pygame, random, sys
 
 BLK = (0, 0, 0)
 WHT = (255, 255, 255)
-WINDOWSIZE = (1024, 768)
+WINDOWSIZE = (800, 600)
 FPS = 60
 BALLSIZE = 10
 BLIP = './blip.wav'
@@ -33,18 +33,18 @@ class Ball:
         self.rect.move_ip(self.dir_x * self.speed, self.dir_y * self.speed)
     
     # BOUNCE IF HIT SOMETHING
-    def bounce(self, axis) -> None:
+    def bounce(self, axis, sound) -> None:
         if axis == 'x':
-            pygame.mixer.music.play()
+            sound.play()
             self.dir_x *= -1
         elif axis == 'y':
-            pygame.mixer.music.play()
+            sound.play()
             self.dir_y *= -1
     
     # ADJUST SPEED
     def ball_speed(self, total) -> None:
         if total != 0 and total % 5 == 0:
-            self.speed *= 1.2
+            self.speed *= 1.1
 
 # PLAYER 1/2
 class Player:
@@ -132,34 +132,51 @@ class TheGame:
     # GAME SETUP
     def __init__(self, width=WINDOWSIZE[0], height=WINDOWSIZE[1]) -> None:
         pygame.init()
-        self.font = pygame.font.SysFont('Ubuntu', 36)
+        self.clock = pygame.time.Clock()
         
+        # FONTS AND SIZES
+        self.title_font = pygame.font.SysFont('Ubuntu', 36)
+        self.text_font = pygame.font.SysFont('Ubuntu', 32)
+        self.score_font = pygame.font.SysFont('Ubuntu', 24)
+        self.help_font = pygame.font.SysFont('Ubuntu', 16)
+        
+        # SCREEN
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption('pyPong')
         
-        self.clock = pygame.time.Clock()
+        # PLAYFIELD
+        # TODO
         
-        pygame.mixer.music.load(BLIP)
+        # AUDIO SETUP
+        pygame.mixer.init()
+        self.sfx_blip = pygame.mixer.Sound(BLIP)
         
     # MAIN MENU
     def menu(self) -> str:
+        """
+        returns chosen game mode (str)
+        """
         # BUTTONS
         button_width = WINDOWSIZE[1] / 2
-        button_height = WINDOWSIZE[0] / 6
+        button_height = WINDOWSIZE[0] / 8
         
         but_1PvsCPU = pygame.Rect(self.screen.get_width() / 2 - button_width / 2,
                                   self.screen.get_height() / 3 - button_height / 2,
                                   button_width, button_height)
         
         but_1Pvs2P = pygame.Rect(self.screen.get_width() / 2 - button_width / 2,
-                                  self.screen.get_height() / 3 + (button_height / 2),
+                                  self.screen.get_height() / 3 + button_height / 2 + BALLSIZE,
+                                  button_width, button_height)
+        
+        but_about = pygame.Rect(self.screen.get_width() / 2 - button_width / 2,
+                                  self.screen.get_height() / 3 + (button_height * 1.5) + 2 * BALLSIZE,
                                   button_width, button_height)
         
         but_quit = pygame.Rect(self.screen.get_width() / 2 - button_width / 2,
-                                  self.screen.get_height() / 3 + (button_height * 1.5),
+                                  self.screen.get_height() / 3 + (button_height * 3) - 2 * BALLSIZE,
                                   button_width, button_height)
         
-        buttons = [(but_1PvsCPU, '1P vs CPU'), (but_1Pvs2P, '1P vs 2P'), (but_quit, 'QUIT')]
+        buttons = [(but_1PvsCPU, '1P vs CPU'), (but_1Pvs2P, '1P vs 2P'), (but_about, 'ABOUT'), (but_quit, 'QUIT')]
         # RUN THE SCREEN
         while True:
             for event in pygame.event.get():
@@ -174,32 +191,92 @@ class TheGame:
                         return '1PvsCPU'
                     if but_1Pvs2P.collidepoint(mouse_pos):
                         return '1Pvs2P'
+                    if but_about.collidepoint(mouse_pos):
+                        self.about()
                     if but_quit.collidepoint(mouse_pos):
                         pygame.quit()
                         sys.exit()
+            
             # FILL WITH COLOUR
             self.screen.fill(BLK)
             # DISPLAY THE TITLE
-            game_title = self.font.render('pyPONG', True, WHT)
+            game_title = self.title_font.render('pyPONG', True, WHT)
             self.screen.blit(game_title, (self.screen.get_width() / 2 - game_title.get_width() / 2, 30))
+            
             # DRAW ALL THE BUTTONS
             for button, label in buttons:
-                pygame.draw.rect(self.screen, WHT, button, 2)
-                text = self.font.render(label, True, WHT)
+                pygame.draw.rect(self.screen, WHT, button, 1)
+                text = self.text_font.render(label, True, WHT)
                 text_rect = text.get_rect(center=button.center)
                 self.screen.blit(text, text_rect)
             
             pygame.display.flip()
-            self.clock.tick(60)
+            self.clock.tick(FPS)
+    
+    # ABOUT SCREEN
+    def about(self) -> None:
+        # BACK BUTTON
+        button_width = WINDOWSIZE[1] / 4
+        button_height = WINDOWSIZE[0] / 8
+        
+        but_back = pygame.Rect(self.screen.get_width() - (button_width + 2 * BALLSIZE),
+                               self.screen.get_height() - (button_height + 2 * BALLSIZE),
+                               button_width, button_height)
+        
+        # GAME DESCRIPTION
+        game_description = '''
+        pyPONG
+        
+        Player 1 controls:
+            up: W    down: S
+        Player 2 controls:
+            up: UP   down: DOWN
+        Press SPACE to pause the game
+        Press ESC to quit
+        
+        The ball speed increases after successful hits
+        The paddles grow after each personal 10th hit
+        '''
+        
+        # THE LOOP
+        while True:
+            for event in pygame.event.get():
+                # CHECK FOR EXIT GAME  
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                # CHECK FOR MOUSE CLICKS
+                if event.type == pygame.MOUSEBUTTONUP:
+                    mouse_pos = pygame.mouse.get_pos()
+                    if but_back.collidepoint(mouse_pos):
+                        return
+            
+            # FILL WITH COLOUR
+            self.screen.fill(BLK)
+            
+            # DRAW THE TEXT DESCRIPTION
+            game_text = self.text_font.render(game_description, True, WHT)
+            game_text_rect = game_text.get_rect()
+            self.screen.blit(game_text, game_text_rect)
+            
+            # DRAW BACK BUTTON
+            pygame.draw.rect(self.screen, WHT, but_back, 1)
+            text = self.text_font.render('BACK', True, WHT)
+            text_rect = text.get_rect(center=but_back.center)
+            self.screen.blit(text, text_rect)
+            
+            pygame.display.flip()
+            self.clock.tick(FPS)
+            
     
     # GAME OVER SCREEN
-    def game_over(self, mode):
+    def game_over(self, mode) -> None:
         # BUTTONS
-        button_width = WINDOWSIZE[1] / 2
-        button_height = WINDOWSIZE[0] / 6
+        button_width = WINDOWSIZE[1] / 3
+        button_height = WINDOWSIZE[0] / 8
         
         but_restart = pygame.Rect(self.screen.get_width() / 2 - button_width / 2,
-                                  self.screen.get_height() / 3 - button_height / 2,
+                                  self.screen.get_height() / 3 - (button_height / 2 + 2 * BALLSIZE),
                                   button_width, button_height)
         
         but_menu = pygame.Rect(self.screen.get_width() / 2 - button_width / 2,
@@ -207,7 +284,7 @@ class TheGame:
                                   button_width, button_height)
         
         but_quit = pygame.Rect(self.screen.get_width() / 2 - button_width / 2,
-                                  self.screen.get_height() / 3 + (button_height * 1.5),
+                                  (self.screen.get_height() / 3 * 2) + (button_height / 2),
                                   button_width, button_height)
         
         buttons = [(but_restart, 'RESTART'), (but_menu, 'MENU'), (but_quit, 'QUIT')]
@@ -233,17 +310,18 @@ class TheGame:
             # FILL WITH COLOUR
             self.screen.fill(BLK)
             # DISPLAY THE TITLE
-            game_over_txt = self.font.render('GAME OVER', True, WHT)
+            game_over_txt = self.title_font.render('GAME OVER', True, WHT)
             self.screen.blit(game_over_txt, (self.screen.get_width() / 2 - game_over_txt.get_width() / 2, 30))
+            
             # DRAW ALL THE BUTTONS
             for button, label in buttons:
-                pygame.draw.rect(self.screen, WHT, button, 2)
-                text = self.font.render(label, True, WHT)
+                pygame.draw.rect(self.screen, WHT, button, 1)
+                text = self.text_font.render(label, True, WHT)
                 text_rect = text.get_rect(center=button.center)
                 self.screen.blit(text, text_rect)
             
             pygame.display.flip()
-            self.clock.tick(60)
+            self.clock.tick(FPS)
     
     # RESTART ROUND
     def restart(self, mode) -> None:
@@ -280,8 +358,13 @@ class TheGame:
             
             # PUASE THE GAME
             if paused == True:
-                paused_text = self.font.render('P A U S E D', False, WHT)
+                # DISPLAY PAUSED TEXT
+                paused_text = self.title_font.render('P A U S E D', False, WHT)
                 self.screen.blit(paused_text, (self.screen.get_width() / 2 - paused_text.get_width() / 2, self.screen.get_height() / 2 - paused_text.get_height() / 2))
+                
+                # DISPLAY OPTIONS
+                paused_text_sub = self.text_font.render('SPACE to continue, ESC to quit', False, WHT)
+                self.screen.blit(paused_text_sub, (self.screen.get_width() / 2 - paused_text_sub.get_width() / 2, self.screen.get_height() / 2 - paused_text_sub.get_height() / 2 + paused_text.get_height()))
                 pygame.display.flip()
                 continue
             
@@ -297,7 +380,7 @@ class TheGame:
             # LOGIC FOR BALL TO MOVE AND BOUNCE OFF WALLS
             self.ball.move()
             if self.ball.rect.top <= 0 or self.ball.rect.bottom >= self.screen.get_height():
-                self.ball.bounce('y')
+                self.ball.bounce('y', self.sfx_blip)
             
             # LOGIC FOR PENALTY FOR PLAYER
             if self.ball.rect.left <= 0:
@@ -311,7 +394,7 @@ class TheGame:
             if self.ball.rect.colliderect(self.player.rect):
                 self.scores[0] += 1
                 self.player.bonus(self.scores[0], self.player.rect.left, self.player.rect.top)
-                self.ball.bounce('x')
+                self.ball.bounce('x', self.sfx_blip)
                 # UPDATE BALL SPEED
                 total_score = self.scores[0] + self.scores[1]
                 self.ball.ball_speed(total_score)
@@ -320,7 +403,7 @@ class TheGame:
             if self.ball.rect.colliderect(self.opponent.rect):
                 self.scores[1] += 1
                 self.opponent.bonus(self.scores[1], self.opponent.rect.left, self.opponent.rect.top)
-                self.ball.bounce('x')
+                self.ball.bounce('x', self.sfx_blip)
                 # UPDATE BALL SPEED
                 total_score = self.scores[0] + self.scores[1]
                 self.ball.ball_speed(total_score)
@@ -337,11 +420,12 @@ class TheGame:
                                 (self.screen.get_width() / 2, self.screen.get_height()))
             
             # DISPLAY SCORES
-            player_score = self.font.render(f'{self.scores[0]}', True, WHT)
-            opponent_score = self.font.render(f'{self.scores[1]}', True, WHT)
+            player_score = self.score_font.render(f'{self.scores[0]}', True, WHT)
+            opponent_score = self.score_font.render(f'{self.scores[1]}', True, WHT)
             
             self.screen.blit(player_score, (self.screen.get_width() / 4 - player_score.get_width() / 2, 20))
             self.screen.blit(opponent_score, ((self.screen.get_width() / 4) * 3 - opponent_score.get_width() / 2, 20))
+            
             
             pygame.display.flip()
             
